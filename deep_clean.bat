ab75ccc43be0345dfee037307d140d5c7e473544
Ana Carolina <82789655+anacarolina4@users.noreply.github.com>
2021-10-25 13:30:13 -0300

    🐛 fix code

--- deep_clean.bat
+++ deep_clean.bat
@@ -1,4 +1,3 @@
-:inicio
 echo off
 cls
 color 4 
@@ -9,50 +8,24 @@ echo.
 echo ==================================================================
 echo.
 echo.
-
 echo [1] Proceed to deep clean
-echo [2] Activate energy plan (ReviOS energyplan)
 echo.
-echo [3] Exit
+echo [2] Exit
 
 set /p option=Type your option: 
 
 if "%option%" == "1" goto op1
 if "%option%" == "2" goto op2
-if "%option%" == "3" goto op3
 
 :op1
+echo cleaning...
 if exist c:\windows\temp\ del /f /s /q c:\windows\temp\ 
 del /f /s /q %temp%\
 del /f /s /q c:\windows\prefetch\
 "C:\WINDOWS\system32\dfrgui" > nul
-goto inicio
+pause
+goto op2
+
 
 :op2
-powercfg /import C:\energyplan\RevisionPlan.pow
-
-powercfg /l > C:\energyplan\i.txt
-
-for /f "tokens=6 skip=4" %%a in (C:\energyplan\i.txt) do echo %%a > c:\energyplan\h.txt
-
-set /p g= < c:\energyplan\h.txt
-
-echo %g%
-
-if exist %g% goto :gr
-
-:gr
-
-powercfg /s %g%
-
-echo %g%
-
-del /f /s /q c:\energyplan\i.txt > nul 
-del /f /s /q c:\energyplan\h.txt > nul
-
-echo let's clean temp files and optimizing local disc
-pause
-goto op1
-
-:op3
 exit > nul
